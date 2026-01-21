231afbdd4357cfcff69bf8fb89ffdffec922aab6
MyBiosenix <mymailbox8162024@gmail.com>
2026-01-21 18:56:05 +0530

    Same

--- frontend/src/subadmin/pages/SLogin.jsx
+++ frontend/src/subadmin/pages/SLogin.jsx
@@ -1,5 +1,5 @@
 import React, { useState } from 'react'
-import '../../admin/styles/login.css'
+import '../../admin/styles/Login.css'
 import { useNavigate } from 'react-router-dom'
 import axios from 'axios';
 
